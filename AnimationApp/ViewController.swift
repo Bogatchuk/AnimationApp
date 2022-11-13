98254be219d3edba28a6a4e80c71db9e71116d0d
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2022-11-13 19:30:30 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Roma Bogatchuk on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
