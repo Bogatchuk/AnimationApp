cf014dd72cc5a4c7d9b8f9f32692bf1920fe6a3f
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2022-11-14 01:02:18 +0300

    add animation with Spring pod

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,16 +5,18 @@
 //  Created by Roma Bogatchuk on 13.11.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var coreAnimationView : UIView!
+    @IBOutlet weak var springAnimattionView: SpringView!
     private var originCoordinate: CGFloat?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         originCoordinate = coreAnimationView.frame.origin.x
+        //springAnimattionView.animate()
     }
 
     @IBAction func startCoreAnimation(_ sender: UIButton) {
@@ -24,11 +26,19 @@ class ViewController: UIViewController {
             if self.coreAnimationView.frame.origin.x == self.originCoordinate {
                 self.coreAnimationView.frame.origin.x -= 40
             }else {
-                self.coreAnimationView.frame.origin.x + = 40
+                self.coreAnimationView.frame.origin.x += 40
             }
             
         }
     }
     
+    @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        springAnimattionView.animation = "wobble"
+        springAnimattionView.duration = 0.5
+        springAnimattionView.curve = "easeIn"
+        springAnimattionView.force = 2
+        springAnimattionView.delay = 0.3
+        springAnimattionView.animate()
+    }
 }
 
